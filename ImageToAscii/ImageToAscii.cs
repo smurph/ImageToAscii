f0bbae4d89bd4d562b150d689d769194e85efe68
Smurph <smurph@users.noreply.github.com>
2016-11-04 03:23:23 -0400

    Second initial commit

---
+++ ImageToAscii/ImageToAscii.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.IO;
+using System.Drawing.Imaging;
+
+namespace ImageToAscii
+{
+    public class ImageToAscii
+    {
+
+        private static string[] _characters = new string[6] { " ", ".", "+", "o", "#", "W" };
+        private static double[] _characterGrayValueThresholds = new double[6] { 220.0, 185.0, 150.0, 115.0, 80.0, 45.0 };
+        
+        public static void ConvertToAscii(string pictureFileName, string outputFileName)
+        {
+            Bitmap b = new Bitmap(pictureFileName);
+
+            StringBuilder output = new StringBuilder((b.Width + 1) * b.Height);
+
+            for (int y = 0; y < b.Height; y++)
+            {
+                if (output.Length > 0) output.Append("\n");
+                for (int x = 0; x < b.Width; x++)
+                {
+                    Color c =  b.GetPixel(x, y);
+
+                    // Luminosity method:
+                    // 0.21 R + 0.72 G + 0.07 B
+                    double grayValue = (c.R * 0.21) + (c.G * 0.72) + (c.B * 0.07);
+
+                    output.Append(getCharacterFromGrayValue(grayValue));
+                }
+            }
+
+
+            File.WriteAllLines(outputFileName, output.ToString().Split('\n'));
+        }
+
+        private static string getCharacterFromGrayValue(double value)
+        {
+            for (int i = 0; i < _characters.Length; i++)
+            {
+                if (value >= _characterGrayValueThresholds[i])
+                {
+                    return _characters[i];
+                }
+            }
+
+            // really dark. 
+            return "@";
+        }
+
+        private byte[] toByteArray(Image img)
+        {
+            MemoryStream ms = new MemoryStream();
+
+            img.Save(ms, ImageFormat.Png);
+
+            return ms.ToArray();
+        }
+    }
+}
---
+++ Test/ImageToAsciiTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Drawing;
+using ImageToAscii;
+using System.IO;
+using System.Linq;
+
+namespace Test
+{
+    [TestClass]
+    public class ImageToAsciiTests
+    {
+        // realy need to change this path reference to be local
+        private string picFileName = "D:\\Code\\ImageToAscii\\Test\\Assets\\dog.jpg";
+        private string _outputFileName = "D:\\Code\\ImageToAscii\\Test\\Assets\\output.txt";
+
+
+        [TestMethod]
+        public void ConvertToAsciiReturnsMoreThanZeroLines()
+        {
+            Bitmap pic = new Bitmap(picFileName);
+
+            ImageToAscii.ImageToAscii.ConvertToAscii(picFileName, _outputFileName);
+            
+            Assert.AreNotEqual(0, File.ReadLines(_outputFileName).ToList().Count);
+        }
+
+        [TestCleanup]
+        public void RemoveOutputFile()
+        {
+            File.Delete(_outputFileName);
+        }
+    }
+}
