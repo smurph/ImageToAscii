adeda3907ae894f812559f5de6e045c1cfc7c087
Smurph <smurph@users.noreply.github.com>
2016-11-05 05:03:11 -0400

    Addes more overloads supporting Bitmaps. Added more tests. Removed character gradient threshold array.

--- ImageToAscii/ImageToAscii.cs
+++ ImageToAscii/ImageToAscii.cs
@@ -1,28 +1,67 @@
 ﻿using System.Text;
 using System.Drawing;
 using System.IO;
-using System.Drawing.Imaging;
+using System.Collections.Generic;
 
-namespace ImageToAscii
+namespace ConvertToAscii
 {
     public static class ImageToAscii
     {
+        /// <summary>
+        ///  After calculating proper scale height of an image based on the provided width
+        ///  scale the height again to account for the fact that pixels are square and 
+        ///  characters are not. Actual proper value will vary per font which isn't handled
+        ///  by this app, but something between 2 and 3 should work fine as a rule of thumb.
+        /// </summary>
+        private const double _verticalScale = 2.5;
 
-        private static string[] _characters = new string[6] { " ", ".", "+", "o", "#", "W" };
-        private static double[] _characterGrayValueThresholds = new double[6] { 220.0, 185.0, 150.0, 115.0, 80.0, 45.0 };
+
+        /// <summary>
+        /// Array of characters that must be arranged from "lightest" to "darkest" that 
+        /// will be used to represent various grayscale values.
+        /// </summary>
+        public static string[] CharacterSet = new string[10] { " ", ".", ":", "-", "=", "+", "*", "#", "%", "@" };
+
         
-        public static void ConvertToAscii(string pictureFileName, string outputFileName)
+        /// <summary>
+        /// Converts an image to ascii with a line width the same as the image width. 
+        /// Writes output to file
+        /// </summary>
+        /// <param name="inputFileName">Fullly qualified filename to any image file</param>
+        /// <param name="outputFileName">Full path to dump the output text to</param>
+        public static void ConvertToAscii(string inputFileName, string outputFileName)
         {
-            Bitmap b = new Bitmap(pictureFileName);
+            ConvertToAscii(inputFileName, outputFileName, 0);
+        }
 
-            StringBuilder output = new StringBuilder((b.Width + 1) * b.Height);
+        /// <summary>
+        /// Converts an image to ascii with a defined line width, and writes to file
+        /// </summary>
+        /// <param name="inputFileName">Fullly qualified filename to any image file</param>
+        /// <param name="outputFileName">Full path to dump the output text to</param>
+        /// <param name="width">Width to scale the image to.</param>
+        public static void ConvertToAscii(string inputFileName, string outputFileName, int width)
+        {
+            Bitmap b = new Bitmap(inputFileName);
+            ConvertToAscii(b, outputFileName, width);
+        }
 
-            for (int y = 0; y < b.Height; y++)
+        public static void ConvertToAscii(Bitmap input, string outputFileName, int width)
+        {
+            if (width <= 0) width = input.Width;
+            
+            int height = (int)(((double)width / input.Width * input.Height) / _verticalScale);
+
+            Bitmap scaledInput = new Bitmap(input, new Size(width, height));
+            
+            StringBuilder output = new StringBuilder((scaledInput.Width + 2) * scaledInput.Height);
+            
+            for (int y = 0; y < scaledInput.Height; y++)
             {
-                if (output.Length > 0) output.Append("\n");
-                for (int x = 0; x < b.Width; x++)
+                if (output.Length > 0) output.Append("\r\n");
+                for (int x = 0; x < scaledInput.Width; x++)
                 {
-                    Color c =  b.GetPixel(x, y);
+                    Color c = scaledInput.GetPixel(x, y);
 
                     // Luminosity method:
                     // 0.21 R + 0.72 G + 0.07 B
@@ -31,21 +70,21 @@ namespace ImageToAscii
                     output.Append(getCharacterFromGrayValue(grayValue));
                 }
             }
-            File.WriteAllLines(outputFileName, output.ToString().Split('\n'));
+            File.WriteAllText(outputFileName, output.ToString());
         }
 
         private static string getCharacterFromGrayValue(double value)
         {
-            for (int i = 0; i < _characters.Length; i++)
+            for (int i = CharacterSet.Length; i >= 0; i--)
             {
-                if (value >= _characterGrayValueThresholds[i])
+                double n = 255.0 - ((255.0 / CharacterSet.Length) * i);
+                if (value < n)
                 {
-                    return _characters[i];
+                    return CharacterSet[i];
                 }
             }
 
-            // really dark. 
-            return "@";
+            return CharacterSet[CharacterSet.Length-1];
         }
     }
 }
---
+++ Test/ConvertToAscii.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Drawing;
+using ConvertToAscii;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Collections.Generic;
+
+namespace Test
+{
+    [TestClass]
+    public class ConvertToAscii
+    {
+        private string _outputFileName = "output.txt";
+        private List<string> _inputFileNames;
+
+        private Dictionary<string, Bitmap> _inputs;
+
+        [TestInitialize]
+        public void Init()
+        {
+            //todo: do a better job at determining the local directory
+            string testRoot = "D:\\Code\\ImageToAscii\\Test\\Assets\\";
+
+            _inputs = new Dictionary<string, Bitmap>();
+            _inputFileNames = new List<string>();
+            _outputFileName = testRoot + _outputFileName;
+
+            foreach (var file in Directory.GetFiles(testRoot, "*.jpg")) 
+            {
+                string[] fileNameParts = file.Split('\\');
+
+                _inputs.Add(fileNameParts[fileNameParts.Length - 1], new Bitmap(file));
+                _inputFileNames.Add(file);
+            }
+            
+            RemoveOutputFile();
+        }
+
+        [TestMethod]
+        public void TwoValidFileNames_WritesMoreThanZeroLines()
+        {
+            ImageToAscii.ConvertToAscii(_inputFileNames[0], _outputFileName);
+            
+            Assert.AreNotEqual(0, File.ReadLines(_outputFileName).ToList().Count);
+        }
+
+        [TestMethod]
+        public void TwoValidFileNamesAndValidWidth_WritesProperScaledWidth()
+        {
+            int width = 100;
+            ImageToAscii.ConvertToAscii(_inputFileNames[0], _outputFileName, width);
+
+            // ReadLines eats the line endings, so no need to account for their width.
+            Assert.AreEqual(width, File.ReadLines(_outputFileName).ToList()[0].Length);
+        }
+
+        [TestMethod]
+        public void BitmapAndFileNameAndValidWidth_WritesSomethingToFile()
+        {
+            int width = 200;
+            ImageToAscii.ConvertToAscii(_inputs["cat.jpg"], _outputFileName, width);
+
+            Assert.IsTrue(File.Exists(_outputFileName));
+            Assert.AreNotEqual(0, File.ReadAllText(_outputFileName).Length);
+        }
+
+        [TestMethod]
+        public void ValidGradientBitmapInput_WritesAllCharacters()
+        {
+            ImageToAscii.ConvertToAscii(_inputs["gradient.jpg"], _outputFileName, 500);
+
+            string outputContents = File.ReadAllText(_outputFileName);
+
+            foreach (var character in ImageToAscii.CharacterSet)
+            {
+                Assert.IsTrue(outputContents.Contains(character));
+            }
+        }
+        
+        [TestCleanup]
+        public void Cleanup()
+        {
+            RemoveOutputFile();
+        }
+
+        private void RemoveOutputFile()
+        {
+            //todo: properly mock objects so we don't write to disk, and instead capture attemps
+            try
+            {
+                File.Delete(_outputFileName);
+            }
+            catch { }
+        }
+    }
+}
